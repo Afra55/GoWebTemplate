47fbf38de46e4ceb7f6e3d376b87fc7ebf19092b
Afra55 <1937525930@qq.com>
2016-09-29 16:00:09 +0800

    IsExist func.

--- main/photoweb.go
+++ main/photoweb.go
@@ -77,8 +77,22 @@ func main() {
 func staticDirHandler(mux *http.ServeMux, prefix string, staticDir string, flags int) {
 	mux.HandleFunc(prefix, func(w http.ResponseWriter, r *http.Request) {
 		file := staticDir + r.URL.Path[len(prefix) - 1:]
+		if (flags & ListDir) == 0 {
+			if exists := IsExist(file); !exists {
+				http.NotFound(w, r)
+				return
+			}
+		}
 		http.ServeFile(w, r, file)
 	})
+}
+
+func IsExist(path string) bool{
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
 }
 
 // 避免方法出错导致程序停止运行
